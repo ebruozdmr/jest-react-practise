b3ac2eaee2718e715f3cef80ccad5829f9827cd9
Ebru Özdemir <ozdemir.ebbru@gmail.com>
2024-07-01 17:17:43 +0300

    files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
 function App() {
+  // const [str, setStr] = useState("software");
+  // const [str2, setStr2] = useState("JEST");
+  const [counter, setCounter] = useState(0);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setStr("JEST");
+  //     setStr2("software");
+  //   }, 300);
+  // }, []);
+
+  // const str = "JEST";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div> {str}</div>
+      <div> {str2}</div>
+      <div title="title">React Testing by using...</div>
+      <div title="title">React Testing by using...</div> */}
+      {/* htmlFor attribute'ı ile ilgili label ve input eşleştirilir. */}
+      {/* <label htmlFor="input">First name</label>
+      <input id="input" type="text" placeholder="Enter the first name"></input>
+      <label htmlFor="input2">second Name</label>
+      <input
+        id="input2"
+        type="text"
+        placeholder="Enter the second name"
+      ></input>
+      <button>Test</button>
+      <button>Test2</button>
+      <h1>Testing React Elements</h1> */}
+      {/* <h1>Testing React</h1> */}
+      {/* <input data-testId="test" value="testing"></input> */}
+      {/* <a href="http://google.com">Click</a>
+      <img src="" alt="img"></img> */}
+      <h1 id="counter">Counter:{counter}</h1>
+      <button onClick={() => setCounter(counter + 2)}>Increase</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,138 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { findByText, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+//import the file which is tested
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//Grouping of the same tests
+describe("text test", () => {
+  //it() is also used instead of test()
+  // test("test with getByText ", () => {
+  //   //render the file which is tested
+  //   render(<App></App>);
+  //   const element = screen.getByText(/React Testing/i); //RegExp ile bu ifadenin text içerisindeki varlığı test edilir.
+  //   //Bu yapı da testi yapılan text'in içerisinde ilgili ifadenin bulunup bulunmadığı ile alakalı bilgi verir.
+  //   // const element = screen.getByText("React Testing", {
+  //   //   exact: false,
+  //   // });
+  //   expect(element).toBeInTheDocument();
+  // });
+  // test("test with getByText 2", () => {
+  //   render(<App></App>);
+  //   /*Burada text'in bütününün "varlığı" test edilir.
+  //     Ayrıca getByText text'in sadece bir tane olduğu durumlarda success döner.
+  //   */
+  //   const element = screen.getByText("React Testing");
+  //   expect(element).toBeInTheDocument();
+  // });
+  // test("test with queryByText ", () => {
+  //   render(<App></App>);
+  //   //Burada text'in bütününün "var olmadığı" test edilir.
+  //   const element = screen.queryByText("React Testing"); //when "no match" case return null
+  //   expect(element).not.toBeInTheDocument();
+  // });
+  // /*JEST ifadesi sayfa yüklendikten yarım saniye sonra arayüzde render edildi.
+  // Bu nedenle async/await yapısı ile kullanılan "findByText" ile test işlemi gerçekleştirilir.
+  // */
+  // test("test of async/await structure", async () => {
+  //   render(<App></App>);
+  //   const element = await screen.findByText("JEST");
+  //   expect(element).toBeInTheDocument();
+  // });
+  // test("testing multimatch text", () => {
+  //   render(<App></App>);
+  //   const element = screen.getAllByText("React Testing by using..."); //return array
+  //   // expect(element[0]).toBeInTheDocument();
+  //   expect(element).toHaveLength(2);
+  // });
+
+  // test("testing of async/await structure 2", async () => {
+  //   render(<App></App>);
+
+  //   await waitFor(
+  //     () => {
+  //       expect(screen.getByText("JEST")).toBeInTheDocument();
+  //     },
+  //     { timeout: 1000 }
+  //   );
+  //   await waitFor(
+  //     () => {
+  //       expect(screen.getByText("software")).toBeInTheDocument();
+  //     },
+  //     { timeout: 1000 }
+  //   );
+  // });
+
+  // test("testing of input value via label tag", () => {
+  //   render(<App></App>);
+  //   /* testing for label tag */
+  //   expect(screen.getByLabelText("React")).toBeInTheDocument(); // select of input tag
+  //   /* testing for input value */
+  //   expect(screen.getByLabelText("React")).toHaveValue("test");
+  // });
+
+  // test("testing of div element", () => {
+  //   render(<App></App>);
+  //   // expect(screen.getAllByTitle("title")[0]).toBeInTheDocument();
+  //   // expect(screen.getAllByRole("button")[0]).toBeInTheDocument();
+  //   // expect(screen.getByRole("heading")).toBeInTheDocument();
+  //   // expect(screen.getAllByRole("heading")[1]).toBeInTheDocument();
+  //   // expect(screen.getByDisplayValue("test")).toBeInTheDocument();
+  //   // expect(screen.getByTestId("test")).toBeInTheDocument();
+  //   // expect(screen.getByText("Click").href).toContain("http");
+  //   // expect(screen.getByAltText("img")).toBeInTheDocument();
+  //   expect(screen.getByRole("textbox")).toBeInTheDocument(); //testing for input tag
+  // });
+  // test("testing of multiple input tags", () => {
+  //   render(<App></App>);
+  //   const firstNameInput = screen.getByRole("textbox", { name: /first name/i }); //label text'i "first name" olan input tag'i
+  //   expect(firstNameInput).toBeInTheDocument();
+  //   expect(firstNameInput).toHaveAttribute(
+  //     "placeholder",
+  //     "Enter the first name"
+  //   );
+
+  //   const lastNameInput = screen.getByRole("textbox", { name: /second name/i }); // i flag: insensitive case
+  //   expect(lastNameInput).toBeInTheDocument();
+  //   expect(lastNameInput).toHaveAttribute("placeholder", "Enter the second name");
+  // });
+  // test("testing for general purposes", () => {
+  //   render(<App></App>);
+  //   expect(2 + 2).toBe(4);
+  //   let user = {
+  //     name: "Ebru",
+  //   };
+  //   user["age"] = 29;
+  //   expect(user).toEqual({ name: "Ebru", age: 29 });
+  // });
+
+  test("testing for user events", async () => {
+    const { container } = render(<App></App>);
+    console.log(container);
+    screen.debug();
+    //userEvent, daha gerçekçi kullanıcı etkileşimlerini simüle etmeye yarar.
+    const user = userEvent.setup();
+    //user events are asyncronous events
+    //pointer represents the mouse events
+    // await user.pointer({
+    //   keys: "[MouseLeft]",
+    //   target: screen.getByRole("button"),
+    // });
+    // expect(screen.getByRole("heading", { name: /Counter/i })).toHaveTextContent(
+    //   "1"
+    // );
+
+    // await user.click(screen.getByRole("button", { name: /Increase/i }));
+    // expect(screen.getByRole("heading", { name: /Counter/i })).toHaveTextContent(
+    //   "2"
+    // );
+    /*container kullanımı ile, DOM elemanlarına doğrudan erişim sağlanabilir.
+    Burada container ile render edilen bileşenin üst düzey DOM elemanına erişilmiş olur.
+    Ancak React Testing Library ise mümkün olduğunca doğal ve kullanıcı odaklı sorgular 
+    kullanılmasını tevsiye eder. Bu nedenle container kullanımı yaygın değildir.
+     */
+
+    const firstChild = container.firstChild;
+    expect(firstChild).toHaveTextContent(/Counter/i);
+    expect(container.querySelector("h1")).toBeInTheDocument();
+  });
 });
